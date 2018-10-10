8779a6b3f875512dd8c50860b1893a3e44a0f071
WIN_NT\x16g027 <x16g027@chiba-fjb.ac.jp>
2018-10-10 10:56:51 +0900

    変更1

--- app/src/main/java/jp/ac/chiba_fjb/x16g_a/test/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/x16g_a/test/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //LoveLive!SunShine!!
-        //松浦果南
+        //コメント
     }
 }
